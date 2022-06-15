1ae385bd8ade6bc2f813cf0b51bcd45a92f25989
johnathanwastaken <danielortiz2001@hotmail.com>
2022-06-14 20:00:06 -0700

    update

--- controllers/languages_controller.js
+++ controllers/languages_controller.js
@@ -2,4 +2,20 @@ const express = require('express')
 const languages = express.Router()
 const Language = require('../models/language.js')
 
+// Index:
+languages.get('/', (req, res) => {
+    Language.find()
+        .then(foundLanguages => {
+            res.json(foundLanguages)
+        })
+})
+
+// Show:
+languages.get('/:name', (req, res) => {
+    Language.findOne({ name: req.params.language .toLowerCase() })
+        .then(foundLanguage => {
+            res.json(foundLanguage)
+        })
+})
+
 module.exports = languages
